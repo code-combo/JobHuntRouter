fb170d48ea9601ab5ab3223093bb72b87aa998b9
code-combo <rjayjutolu419@gmail.com>
2023-10-01 19:05:04 +0100

    Added loaders to the CareersLayout route and component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import RootLayout from "./layouts/RootLayout"
 import About from "./pages/About"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import CareersLayout from "./layouts/CareersLayout"
+import Careers, { careersLoader } from "./pages/careers/Careers"
+import CareerDetails from "./pages/careers/CareerDetails"
 
 
 // Routes
@@ -18,11 +21,24 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path = "/" element = {<RootLayout />}>
       <Route index element = {<Home/>}></Route>
-      <Route path = "/about" element = {<About/>}></Route>
+      <Route path = "about" element = {<About/>}></Route>
+
       <Route path="help" element = {<HelpLayout/>}>
         <Route path = "faq" element = {<Faq/>}/>
         <Route path = "contact" element = {<Contact/>}/>
       </Route>
+
+      <Route path="careers" element = {<CareersLayout/>}>
+        <Route 
+          index 
+          element = {<Careers/>}
+          loader={careersLoader}
+        />
+      </Route>
+      <Route
+        path=":id"
+        element = {<CareerDetails/>}
+      />
 
       <Route path="*" element = {<NotFound/>}/>
     </Route>
